dc0057ca863c3c4bcfc1efedd6430b8b1404dd6c
satyakamtyagi369 <satyakamtyagi8979@gmail.com>
2025-07-06 16:25:30 +0530

    add login signup pages

---
+++ backend/models/User.js
@@ -0,0 +1,7 @@
+const mongoose=require('mongoose');
+const userschema=new mongoose.Schema({
+    name:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true}
+});
+module.exports = mongoose.model('User',userschema);
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,55 @@
 const express= require("express");
 const path = require("path");
-
+const mongoose =require('mongoose');
+const bodyparser = require('body-parser');
+const User = require('./models/User');
 const port = 3000;
 const app = express();
+mongoose.connect("mongodb://localhost:27017/celebalFly",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>console.log("MongoDB connected."))
+.catch((err)=> console.log(err));
+
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.use(express.static(path.join(__dirname,'../frontend/public')));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'../frontend/public/index.html'));
 })
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../frontend/public/signup.html'));
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../frontend/public/login.html'));
+})
 
+//signup page handle karna hai
+app.post('/signup',async(req,res)=>{
+    try{
+        const{name,email,password}=req.body;
+        if(!name||!email||!password){
+            return res.redirect('/signup?error=missinng');
+        }
+        await User.create({name,email,password});
+        res.redirect('/login');
+    }
+    catch(err){
+        console.log(err);
+        res.redirect('/signup?error=exists');
+    }
+});
+//login page ab handle karna hai.
+app.post('/login',async(req,res)=>{
+    const {name,password}=req.body;
+    const user = await User.findOne({name,password});
+    if(!user){
+        //user nahi hai, toh pehle signup karo.
+        return res.redirect('/signup?error=missing');
+    }
+    res.redirect('/');
+})
 app.listen(port,()=>{
     console.log(`Server is listening on http://localhost:${port}`);
 })
